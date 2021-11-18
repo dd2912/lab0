19f856e6456aaba46bd4d5adf9450b40e64cba7c
Dave <dd2912@columbia.edu>
2021-11-18 10:39:59 -0500

    init

---
+++ lab1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo -e "================================================================================"
+echo -e "                                Welcome to CS377                                "
+echo -e "================================================================================"
+echo -e "-------------------------------System Information-------------------------------"
+echo -e "User:\t\t\t"`whoami`
+echo -e "Hostname:\t\t"`hostname`
+echo -e "uptime:\t\t\t"`uptime | awk '{print $3,$4}' | sed 's/,//'`
+echo -e "Manufacturer:\t\t"`cat /sys/class/dmi/id/chassis_vendor`
+echo -e "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
+echo -e "Machine Type:\t\t"`vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi`
+echo -e "Operating System:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
+echo -e "Kernel:\t\t\t"`uname -r`
+echo -e "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
+echo ""
+echo -e "-------------------------------CPU/Memory Usage-------------------------------"
+echo -e "Memory Usage:\t"`free | awk '/Mem/{printf("%.2f%"), $3/$2*100}'`
+echo -e "Swap Usage:\t"`free | awk '/Swap/{printf("%.2f%"), $3/$2*100}'`
+echo -e "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
+echo -e "------------------------------------------------------------------------------"
+echo ""
